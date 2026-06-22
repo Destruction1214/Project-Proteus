4110bf2b90d980f7ef945043b68e637621fd490f
Supernova-TR-ProPro <nirh95@gmail.com>
2026-06-22 18:28:41 +0200

    Update Fighter.lua

--- Data/Scripts/Library/standard-fighters/Fighter.lua
+++ Data/Scripts/Library/standard-fighters/Fighter.lua
@@ -117,6 +117,8 @@ return {
 			X1 = {"HOWLRUNNER_SQUADRON", false},
 			PRAJI = {"A9_SQUADRON", false},
 			BALMORRA = {"Z95ML_HEADHUNTER_SQUADRON", false},
+			RENDILI = {"TOSCAN_MISSILE_SQUADRON", false},
+
 		}
 		
 		if owner == "IMPERIAL_PROTEUS" then
