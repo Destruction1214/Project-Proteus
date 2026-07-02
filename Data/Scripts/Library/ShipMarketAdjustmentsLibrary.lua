c316b66b970d2ae2cfc457152d46d91b58657675
Destruction1214 <249353461+Destruction1214@users.noreply.github.com>
2026-07-02 20:29:17 +0200

    ShipMarketAdjustmentsLibrary Notes Update

--- Data/Scripts/Library/ShipMarketAdjustmentsLibrary.lua
+++ Data/Scripts/Library/ShipMarketAdjustmentsLibrary.lua
@@ -4,10 +4,10 @@ return {
 		--		{"IMPERIAL_PROTEUS", "SHIP_MARKET", "UNIT_NAME", state}, -- state: false to unlock, true to lock 
 		--	},
 		--	requirement_lists = {
-		--		{"IMPERIAL_PROTEUS", "SHIP_MARKET", "UNIT_NAME", ""}, -- Description: "" = None
+		--		{"IMPERIAL_PROTEUS", "SHIP_MARKET", "UNIT_NAME", ""}, -- Description; "" = None
 		--	},
 		--	adjustment_lists = {
-		--		{"IMPERIAL_PROTEUS", "SHIP_MARKET", "UNIT_NAME", X}, -- X can be positive and negative integer
+		--		{"IMPERIAL_PROTEUS", "SHIP_MARKET", "UNIT_NAME", X}, -- X can be positive and negative integer; adding optional ', true' after X sets X as new chance
 		--	}
 		--},
 		["DRAGON"] = {
