da4a7aa8486d6bb50b7f0ce7b01fd51f83904086
Destruction1214 <249353461+Destruction1214@users.noreply.github.com>
2026-07-11 22:31:00 +0200

    Fixes
    -added loop for nova
    -added X1 Esec upgrade
    -added shuttle for X1 and hethrir

--- Data/Scripts/Library/GameObjectList.lua
+++ Data/Scripts/Library/GameObjectList.lua
@@ -943,5 +943,6 @@ return {
 ["DUMMY_RANDOM_UNIT_RENDILI_NSBC"] = true,
 ["DUMMY_RANDOM_UNIT_RENDILI_VSD"] = true,
 ["DUMMY_RANDOM_UNIT_RENDILI_GSD"] = true,
+-- ["X1_EXECUTOR_DUMMY"] = true,
   
 }
