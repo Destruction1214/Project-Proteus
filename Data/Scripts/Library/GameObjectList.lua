01566d9bb884f7632984b28d6a04adb8d256cc34
Eleian <129484591+Eleian-naielE@users.noreply.github.com>
2026-07-12 22:14:11 +0900

    Eleian hero added (#333)
    * Eleian hero added

--- Data/Scripts/Library/GameObjectList.lua
+++ Data/Scripts/Library/GameObjectList.lua
@@ -944,6 +944,8 @@ return {
 ["DUMMY_RANDOM_UNIT_RENDILI_VSD"] = true,
 ["DUMMY_RANDOM_UNIT_RENDILI_GSD"] = true,
 -- ["X1_EXECUTOR_DUMMY"] = true,
+-- Project Proteus Dev Heroes
 ["DESTRUCTION1214"] = true, 
 ["ROOK_FROSTFIRE"] = true, 
+["ELEIAN_BLACKHOLESUN"] = true, 
 }
