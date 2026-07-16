8266b7f60898e396916d29ed35c59f0618a0c4f1
Rook427 <emm.1818@hotmail.com>
2026-07-16 12:15:02 -0400

    Finalization

--- Data/Scripts/Library/eawx-mod-icw/gameobjects/SUPERNOVA_VIBES.lua
+++ Data/Scripts/Library/eawx-mod-icw/gameobjects/SUPERNOVA_VIBES.lua
@@ -2,7 +2,7 @@ return {
 	Ship_Crew_Requirement = 55,
 	Fighters = {
 		["ELITE_SHIELDED_ARMORED_INTERCEPTOR_SQUADRON"] = {
-			DEFAULT = {Initial = 1, Reserve = 1}
+			DEFAULT = {Initial = 1, Reserve = 0}
 		}
 	},
 	Native = "IMPERIAL",
