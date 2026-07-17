93e09a8b878d06a5f2b493e95ed584e35e1d2d43
Destruction1214 <249353461+Destruction1214@users.noreply.github.com>
2026-07-17 20:02:43 +0200

    Gamble Fighters Fix
    i has been stoopid, here the fix for it

--- Data/Scripts/Library/standard-fighters/LIGHT_FIGHTER.lua
+++ Data/Scripts/Library/standard-fighters/LIGHT_FIGHTER.lua
@@ -99,7 +99,7 @@ return {
 			if owner == "IMPERIAL_PROTEUS" then
 				local group_name = GlobalValue.Get("PROTEUS_GROUP_NAME")
 				if proteustypes[group_name] then
-					if string.find(fighter, "GAMBLE_") then
+					if string.find(proteustypes[group_name][1], "GAMBLE_") then
 						local random_list = require("random-fighters/GAMBLE_LIGHT_FIGHTER")
 						if random_list[group_name] then
 							if table.getn(random_list[group_name]) > 0 then
