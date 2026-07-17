977bdc79ec14d69c6bc276bc117d8ee28903c174
Destruction1214 <249353461+Destruction1214@users.noreply.github.com>
2026-07-17 21:02:21 +0200

    Gamble Fighter Additional Params
    -adds Min/Max Era and required FighterResearch as conditions for the fighters to be valid

--- Data/Scripts/Library/random-fighters/GAMBLE_BLASTBOAT.lua
+++ Data/Scripts/Library/random-fighters/GAMBLE_BLASTBOAT.lua
@@ -1,5 +1,12 @@
 return {
+    --Min/Max_Era Values required, can be any integer
+    --Research optional, if not needed put 'nil'
+    --Research is the FighterResearch that has to be set for that Fighter to be added to the pool
+
     --["PROTEUS_GROUP_NAME"] = {
-        --list of options, Squadron Name in caps
+        --["OPTION1"] = {Min_Era = X, Max_Era = X, Research = "FighterResearch"},
+        --["OPTION2"] = {Min_Era = X, Max_Era = X, Research = "FighterResearch"},
+        --["OPTION3"] = {Min_Era = X, Max_Era = X, Research = "FighterResearch"},
+        --...
     --},
 }
--- Data/Scripts/Library/random-fighters/GAMBLE_LIGHT_FIGHTER.lua
+++ Data/Scripts/Library/random-fighters/GAMBLE_LIGHT_FIGHTER.lua
@@ -1,15 +1,48 @@
 return {
+    --Min/Max_Era Values required, can be any integer
+    --Research optional, if not needed put 'nil'
+    --Research is the FighterResearch that has to be set for that Fighter to be added to the pool
+
+    --["PROTEUS_GROUP_NAME"] = {
+        --["OPTION1"] = {Min_Era = X, Max_Era = X, Research = "FighterResearch"},
+        --["OPTION2"] = {Min_Era = X, Max_Era = X, Research = "FighterResearch"},
+        --["OPTION3"] = {Min_Era = X, Max_Era = X, Research = "FighterResearch"},
+        --...
+    --},
+
     ["IMPERIAL_LIANNA"] = {
-    "TIE_FIGHTER_SQUADRON",
-    "TIE_STARFIGHTER_SQUADRON",
-    "MISSILE_TIE_FIGHTER_SQUADRON",
-    "TIE_X3_SQUADRON",
-    "SHIELDED_TIE_FIGHTER_SQUADRON",
-    "TIE_DROID_SQUADRON",
-    "TIE_FIGHTER_BF2_SQUADRON",
-    "SHIELDED_MISSILE_TIE_FIGHTER_SQUADRON",
-    "TWIN_ION_ENGINE_STARFIGHTER_SQUADRON",
-    "TIE_POD_SQUADRON",
-	"SUPER_TIE_SQUADRON",
+        ["TIE_FIGHTER_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["TIE_STARFIGHTER_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["MISSILE_TIE_FIGHTER_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["TIE_X3_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["SHIELDED_TIE_FIGHTER_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["TIE_DROID_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["TIE_FIGHTER_BF2_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["SHIELDED_MISSILE_TIE_FIGHTER_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["TWIN_ION_ENGINE_STARFIGHTER_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["TIE_POD_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
+        ["SUPER_TIE_SQUADRON"] = {
+            Min_Era = 1, Max_Era = 20, Research = nil
+        },
     },
 }
