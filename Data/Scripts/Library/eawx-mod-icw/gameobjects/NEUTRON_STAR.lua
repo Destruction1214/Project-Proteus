94af59f5de153c6d68e176e1fad40dcdfe22edab
Supernova-TR-ProPro <nirh95@gmail.com>
2026-07-21 22:30:29 +0200

    Add files via upload

---
+++ Data/Scripts/Library/eawx-mod-icw/gameobjects/NEUTRON_STAR.lua
@@ -0,0 +1,13 @@
+return {
+	Ship_Crew_Requirement = 110,
+	Fighters = {
+		["INTERCEPTOR_HALF"] = {
+			DEFAULT = {Initial = 1, Reserve = 2}
+		},
+		["BOMBER2_HALF"] = {
+			DEFAULT = {Initial = 1, Reserve = 2}
+		}
+	},
+	Native = "CORPORATE_SECTOR",
+	Scripts = {"multilayer", "fighter-spawn"}
+}
