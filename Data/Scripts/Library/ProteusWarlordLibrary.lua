d02aab818206ee9c27cc5e4f262eb721a1dc519a
TaxEvader6686 <garpur2911@gmail.com>
2026-07-22 23:00:20 +0000

    Some Coruscant Proteus work

--- Data/Scripts/Library/ProteusWarlordLibrary.lua
+++ Data/Scripts/Library/ProteusWarlordLibrary.lua
@@ -1306,9 +1306,9 @@ return {
 		["SECRET"] = {
 			FriendlyName = "The Coruscant Proteus you were asking for",
 			Planets = {"CORUSCANT"},
-			LeaderTable = {"TAXEVADER_SYNTAX_ERROR"},
+			LeaderTable = {"TAXEVADER_SYNTAX_ERROR", "TAXEVADER_DREAM_OF_A_QUIET_LIFE"},
 			LeaderEndingNames = {"The Ever Brilliant TaxEvader"},
-			HeroList = {"TaxEvader_SynTax_Error", "Destruction1214","Rook_Frostfire", "Supernova_Vibes", "Eleian_BlackHoleSun"}, --(???), Nova, Michael, Coyote
+			HeroList = {"TaxEvader_SynTax_Error", "TaxEvader_Dream_Of_A_Quiet_Life", "Destruction1214","Rook_Frostfire", "Supernova_Vibes", "Eleian_BlackHoleSun"}, --(???), Nova, Michael, Coyote
 			-- UnlockList = {
 			-- 	-- Space
 
